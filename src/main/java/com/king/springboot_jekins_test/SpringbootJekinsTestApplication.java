360d54fca0c194d40accc043ac6b2d26e1b1e693
king <daytimedreamer@163.com>
2021-08-17 14:42:19 +0800

    springboot初始工程

---
+++ src/main/java/com/king/springboot_jekins_test/SpringbootJekinsTestApplication.java
@@ -0,0 +1,13 @@
+package com.king.springboot_jekins_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJekinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJekinsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/king/springboot_jekins_test/SpringbootJekinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.king.springboot_jekins_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJekinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
